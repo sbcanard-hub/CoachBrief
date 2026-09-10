7d3330a6fdf08445fd359db1cdde508b16bb9928
sbcanard-hub <sbcanard@gmail.com>
2026-09-10 13:32:00 +0200

    Charge les styles tactiques

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import './tactics.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
